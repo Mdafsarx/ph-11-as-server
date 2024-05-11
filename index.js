b550f489db1bf0cf1d42f4b6a9667ddf86caebba
Mdafsarx <mdafsar99009@gmail.com>
2024-05-11 21:35:24 +0600

    wishlist api created done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const cors=require('cors');
+const cors = require('cors');
 require('dotenv').config()
-const app=express();
-const port=process.env.PORT||2000;
+const app = express();
+const port = process.env.PORT || 2000;
 
 app.use(cors())
 app.use(express.json())
@@ -26,22 +26,45 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // Send a ping to confirm a successful connection
-    
+
     const blogsData = client.db("blogsDB").collection('blogs');
+    const wishlist = client.db("blogsDB").collection('wishlist');
 
 
 
-    // blogs data 
-    app.get('/blogs',async(req,res)=>{
-        const cursor = blogsData.find();
-        const result=await cursor.toArray();
-        res.send(result);
+    // blogs get
+    app.get('/blogs', async (req, res) => {
+      const cursor = blogsData.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-    app.post('/blogs',async(req,res)=>{
-      
+    // blogs post
+    app.post('/blogs', async (req, res) => {
+      console.log(req.body)
+      const result = await blogsData.insertOne(req.body);
+      res.send(result);
     })
 
+
+    app.post('/wishlist', async (req, res) => {
+      const result = await wishlist.insertOne(req.body);
+      res.send(result);
+    })
+
+    app.get('/wishlist', async (req, res) => {
+      const cursor = wishlist.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/wishlist/:email', async(req, res) => {
+      console.log(req.params.email)
+      const filter={email:req.params.email}
+      const cursor = wishlist.find(filter);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
 
 
@@ -67,11 +90,11 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('ph-11-as-server is running')
+app.get('/', (req, res) => {
+  res.send('ph-11-as-server is running')
 })
 
 
-app.listen(port,()=>{
-    console.log('server running successful')
+app.listen(port, () => {
+  console.log('server running successful')
 })
